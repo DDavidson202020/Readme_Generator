f320983b387e09f172482993aa80889d5d2dbde6
Yoom Davidson <ddavidson202020@gmail.com>
2020-03-28 12:03:00 -0400

    Add more files

---
+++ index.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+const axios = require('axios').default;
+const api = require("./api");
+const generateMarkdown = require("./generateMarkdown");
+
+const questions = [
+    // questions to user using "enquirer"
+    ];
+    
+    function init() {
+    
+    // Logic for this app shoud be here. You can create more functions if you like. This is just a template for you to start your Homework but it is not required for you to use it.
+    
+    }
+    
+    init();
